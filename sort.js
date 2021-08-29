091581edd1fb250c860fe681e384b68f4d250b34
holynova <holy_nova@126.com>
2021-08-29 19:01:48 +0800

    修复快排算法

--- sort.js
+++ sort.js
@@ -1,7 +1,65 @@
 const { log } = require("./utils/debug");
-const { genCase, it } = require("./utils/sortTest");
+const { genCase, it, testSort } = require("./utils/sortTest");
 
 const utils = {};
+
+function mergeSort(arr) {
+  function merge(leftArr = [], rightArr = []) {
+    // log(leftArr, rightArr);
+    let res = [];
+    let p = 0;
+    let q = 0;
+
+    while (true) {
+      let pValid = p < leftArr.length;
+      let qValid = q < rightArr.length;
+
+      if (pValid && qValid) {
+        if (leftArr[p] < rightArr[q]) {
+          res.push(leftArr[p]);
+          p++;
+        } else {
+          res.push(rightArr[q]);
+          q++;
+        }
+      } else if (pValid) {
+        res.push(leftArr[p]);
+        p++;
+      } else if (qValid) {
+        res.push(rightArr[q]);
+        q++;
+      } else {
+        break;
+      }
+    }
+    return res;
+  }
+
+  function sort(list) {
+    // if (list.length < 5) {
+    //   log(`sort,l=${l},r=${r},list=${list}`);
+    // } else {
+    //   log(`sort,l=${l},r=${r}`);
+    // }
+    if (list.length <= 1) {
+      return list;
+    }
+    let mid = Math.floor((0 + list.length) / 2);
+    let leftArr = list.splice(0, mid);
+    let rightArr = list;
+    return merge(sort(leftArr), sort(rightArr));
+    // return merge(sort(list, l, mid), sort(list, mid + 1, r));
+  }
+
+  // return merge([1, 3, 5], [2, 4, 6]);
+
+  if (arr.length <= 1) {
+    return arr;
+  }
+  return sort(arr);
+  // return sort([3, 2, 1], 0, 3);
+  // return sort(arr, 0, arr.length);
+}
 
 function quickSort(arr) {
   function swap(list, l, r) {
@@ -10,10 +68,10 @@ function quickSort(arr) {
 
   // 包括l, 不包括r
   function partition(list, l, r) {
-    let p = list[r - 1];
+    let lastValue = list[r - 1];
     let index = l;
     for (let i = l; i < r - 1; i++) {
-      if (list[i] < p) {
+      if (list[i] < lastValue) {
         swap(list, index, i);
         index += 1;
       }
@@ -22,31 +80,23 @@ function quickSort(arr) {
     return index;
   }
 
-  let pivot = partition(arr, 0, arr.length);
-  partition(arr, 0, pivot);
-  partition(arr, pivot + 1, arr.length);
+  function sort(list, l, r) {
+    if (l >= r) {
+      return list;
+    }
+    let pivot = partition(list, l, r);
+    sort(list, 0, pivot);
+    sort(list, pivot + 1, r);
+  }
+
+  sort(arr, 0, arr.length);
   return arr;
 }
 
-function testSort(n = 99999, sortMethod) {
-  let right = 0;
-  let wrong = 0;
-  let wrongCase = [];
-  for (let i = 0; i < n; i++) {
-    let inputs = genCase(100, -1000, 1000);
-
-    if (it(inputs, quickSort(inputs))) {
-      right++;
-    } else {
-      wrongCase.push(inputs);
-      wrong++;
-    }
-  }
-  log(`通过${right}, 未通过${wrong}, 通过率${Math.floor((right * 100) / n)}%`);
-}
-
 function main() {
-  testSort();
+  // log("结果", mergeSort([3, 2, 1]));
+  testSort(quickSort, 1000, false);
+  // testSort(mergeSort, 9999, true);
 }
 
 main();
--- utils/sortTest.js
+++ utils/sortTest.js
@@ -7,15 +7,49 @@ function genCase(n = 100, min = 0, max = 100) {
   });
 }
 
-function it(arr, res) {
-  let rightAns = arr.sort((a, b) => a - b).join(",");
+function it(arr, res, showLog = false) {
+  let clone1 = [...arr];
+  // let clone2 = [...arr];
+  let rightAns = clone1.sort((a, b) => a - b).join(",");
+  let isRight = rightAns === res.join(",");
+  if (showLog) {
+    log(`测试结果:${isRight ? "正确" : "错误"}`);
+    if (!isRight) {
+      log("正确结果", rightAns);
+      log("目前结果", res.join(","));
+    }
+  }
+  return isRight;
+}
 
-  // log(`测试结果:${rightAns === res.join(",") ? "正确" : "错误"}`);
-  // log("结果", rightAns);
-  return rightAns === res.join(",");
+function testSort(sortMethod, times = 100, showLog = false) {
+  if (typeof sortMethod !== "function") {
+    throw new Error("sortMethod不是函数");
+    return;
+  }
+
+  let correct = 0;
+  let error = 0;
+  let errorCases = [];
+  for (let i = 0; i < times; i++) {
+    let inputs = genCase(10, -1000, 1000);
+    let clone = [...inputs];
+    if (it(inputs, sortMethod(clone), showLog)) {
+      correct++;
+    } else {
+      errorCases.push(inputs);
+      error++;
+    }
+  }
+  log(
+    `通过${correct}, 未通过${error}, 通过率${Math.floor(
+      (correct * 100) / times
+    )}%`
+  );
 }
 
 module.exports = {
   genCase,
   it,
+  testSort,
 };
